aeabeb67ffb8c58504b5aab15132a46782e7e8b3
Yue <kangyue9323@gmail.com>
2025-07-24 22:25:40 +1000

    initial push

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,9 +1,24 @@
 const { httpGet } = require('./mock-http-interface');
 
+const createRequest = async(url) => {
+  try{
+    const response = await httpGet(url);
+    const {message} = JSON.parse(response.body);
+    if (response.status === 200) {
+      return {'Arnie Quote':message};
+    }else {
+      return{'FAILURE':message};
+    }
+  }catch (error) {
+    console.error('Error fetching Arnie quote:', error);
+    return {'FAILURE': 'Error fetching quote'};
+  }
+}
+
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
-};
+  const promises = urls.map(url => createRequest(url));
+  return Promise.all(promises);
+}
 
 module.exports = {
   getArnieQuotes,
